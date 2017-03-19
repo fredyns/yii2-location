d8ec9245d6325ca1f838af38568fb72062cec857
fredy <email@fredyns.net>
2017-03-19 12:01:20 +0700

    restructuring DB

--- development/dbchangelog/201703182242_create-relationship.sql
+++ development/dbchangelog/201703182242_create-relationship.sql
@@ -1,40 +1,45 @@
 
-ALTER TABLE `regionArea`
-ADD CONSTRAINT `fk_rgnName_type`
+ALTER TABLE `rgn_area`
+ADD CONSTRAINT `fkRgn_name_type`
   FOREIGN KEY (`type_id`)
-  REFERENCES `regionType` (`id`)
+  REFERENCES `rgn_type` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION,
-ADD CONSTRAINT `fk_rgnName_parent`
+ADD CONSTRAINT `fkRgn_name_partof`
   FOREIGN KEY (`part_of`)
-  REFERENCES `regionArea` (`id`)
+  REFERENCES `rgn_area` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-ALTER TABLE `regionArea_lang`
-ADD CONSTRAINT `fk_rgnNameLang_name`
+ALTER TABLE `rgn_area_lang`
+ADD CONSTRAINT `fkRgn_nameLang_name`
   FOREIGN KEY (`area_id`)
-  REFERENCES `regionArea` (`id`)
+  REFERENCES `rgn_area` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-ALTER TABLE `regionType_lang`
-ADD CONSTRAINT `fk_rgnTypeLang_type`
+ALTER TABLE `rgn_type_lang`
+ADD CONSTRAINT `fkRgn_typeLang_type`
   FOREIGN KEY (`type_id`)
-  REFERENCES `regionType` (`id`)
+  REFERENCES `rgn_type` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-ALTER TABLE `regionPartCount`
-ADD CONSTRAINT `fk_rgnChildrenCount_type`
+ALTER TABLE `rgn_partCount`
+ADD CONSTRAINT `fkRgn_partCount_type`
   FOREIGN KEY (`type_id`)
-  REFERENCES `regionType` (`id`)
+  REFERENCES `rgn_type` (`id`)
+  ON DELETE NO ACTION
+  ON UPDATE NO ACTION,
+ADD CONSTRAINT `fkRgn_partCount_partof`
+  FOREIGN KEY (`part_of`)
+  REFERENCES `rgn_area` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
 
-ALTER TABLE `regionPostcode`
-ADD CONSTRAINT `fk_rgnPostcode_name`
+ALTER TABLE `rgn_postcode`
+ADD CONSTRAINT `fkRgn_postcode_area`
   FOREIGN KEY (`area_id`)
-  REFERENCES `regionArea` (`id`)
+  REFERENCES `rgn_area` (`id`)
   ON DELETE NO ACTION
   ON UPDATE NO ACTION;
